ae620ddc7009c152182225744389f83f8fc49726
Jona Joy <jonajoy142@gmail.com>
2023-03-14 00:24:29 +0530

    frontend added!

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,17 @@ pragma solidity >=0.5.0 < 0.9.0;
 
 contract TodoList{
    uint public taskCount = 0;
+   struct Task{
+      uint  id;
+      string content;
+      bool completed; 
+   }
+   mapping(uint=>Task) public tasks;
+   function createTask(string memory _content) public{
+      taskCount++;
+      tasks[taskCount] = Task(taskCount, _content, false);
+   }
+   constructor() public{
+      createTask("Check out dappuniversity.com");
+   }
 }
